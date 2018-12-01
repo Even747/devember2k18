b5703dc3af1a9c10d9431db3b7852192f9ee7142
even <even747@live.com>
2018-12-01 22:29:31 +0200

    First python

---
+++ first.py
@@ -0,0 +1,22 @@
+# first, lets say hello
+
+print "Why hello there, how are you doing?"
+
+# then lets ask some random numer from user
+
+num = input("Could think some number, integer to be precise, and then write it down, please?")
+
+# then the program will cheat and look what the number was and print it out
+
+if num == int:
+	print("Was your number", num)
+else:
+	print("Please try again")	
+
+ans = input("Was I right? y/n")
+
+if ans == "y":
+	print("Well, this must be magic :)")
+else:
+	print("I think someone is lying. Thats naughty!")
+
---
+++ second.py
@@ -0,0 +1,14 @@
+# lets say hello
+print("Hello!")
+
+# next lets ask a password
+
+pw = raw_input("Write down a password, please: ")
+
+# what gets printed out depends on the input
+
+if pw == "foo":
+ print("You may continue on your adventure...")
+else:
+ print("Please try again...")
+
