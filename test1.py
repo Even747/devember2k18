0423315cb433c8022e4f2b4e8232a5f38c233669
Even747 <asagi44@outlook.com>
2018-12-06 21:12:52 +0200

    more testing

--- test1.py
+++ test1.py
@@ -10,3 +10,13 @@ print ("This is some random number: ", counter)
 print ("These are the miles", name, "has travelled", miles)
 print ("Name of the brave traveller: ", name)
 
+list1 = ['Car', 'Bike', 'Boat', 'Skateboard', 'Bus', 'Plane', 'Ship'];
+list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+list3 = ['a','b','c'];
+list4 = ['Berlin', 'London', 'Tokyo', 'Sofia', 'Helsinki', 'Paris', 'Canberra'];
+list5 = ['Foo', 'Blaa', 42, 747, 'idk wtf'];
+
+print (list4[0:5])
+print (list2[1:10])
+print (list5[1,3,5]) #find out how to access only certain list entries
+
